5a4737b44e616b44df91c5b5774052914bc8180c
Anthony Rosario <ajrosario08@gmail.com>
2015-11-03 21:22:46 -0800

    prepared segues for use

--- Psychologist/HappinessViewController.swift
+++ Psychologist/HappinessViewController.swift
@@ -43,7 +43,7 @@ class HappinessViewController: UIViewController, FaceViewDataSource {
     }
     
     func updateUI() {
-        faceView.setNeedsDisplay()
+        faceView?.setNeedsDisplay()
     }
     
     func smilinessForFaceView(sender: FaceView) -> Double? {
