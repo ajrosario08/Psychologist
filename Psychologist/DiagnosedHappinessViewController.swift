56470996efd19574e6d08a7f6aecb8e51780eef3
Anthony Rosario <ajrosario08@gmail.com>
2015-11-04 21:53:37 -0800

    prepared for segue

---
+++ Psychologist/DiagnosedHappinessViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiagnosedHappinessViewController.swift
+//  Psychologist
+//
+//  Created by Anthony Rosario on 11/4/15.
+//  Copyright © 2015 Anthony Rosario. All rights reserved.
+//
+
+import UIKit
+
+class DiagnosedHappinessViewController: HappinessViewController {
+
+    var diagnosticHistory = [Int]()
+    
+    private struct History {
+        static let SegueIdentifier = "Show Diagnostic History"
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case History.SegueIdentifier:
+                if let tvc = segue.destinationViewController as? TextViewController {
+                    tvc.text = "\(diagnosticHistory)"
+                }
+            default: break
+            }
+        }
+    }
+}
