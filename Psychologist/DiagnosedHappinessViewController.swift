b5020fd4f93abce4e86e5c14735b46c783cf4c08
Anthony Rosario <ajrosario08@gmail.com>
2015-11-04 22:01:27 -0800

    made diagnostic History save to NSUsersDefaults

--- Psychologist/DiagnosedHappinessViewController.swift
+++ Psychologist/DiagnosedHappinessViewController.swift
@@ -16,10 +16,17 @@ class DiagnosedHappinessViewController: HappinessViewController {
         }
     }
 
-    var diagnosticHistory = [Int]()
+    private let defaults = NSUserDefaults.standardUserDefaults()
+    
+    var diagnosticHistory: [Int] {
+        get { return defaults.objectForKey(History.DefaultsKey) as? [Int] ?? [] }
+        
+        set { defaults.setObject(newValue, forKey: History.DefaultsKey) }
+    }
     
     private struct History {
         static let SegueIdentifier = "Show Diagnostic History"
+        static let DefaultsKey = "DiagnosedHappinessViewController.History"
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
